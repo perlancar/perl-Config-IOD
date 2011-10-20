04378c7d121b44a89e626f5bda83aae52cc634d5
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-10-20 19:58:45 +0700

    Use $CWD instead of push \@CWD (didn't work)

--- lib/Config/Ini/OnDrugs.pm
+++ lib/Config/Ini/OnDrugs.pm
@@ -150,16 +150,19 @@ sub _include {
     if ($eval_err) {
         $self->_dieline("Can't load file $filename: $eval_err");
     }
-    push @CWD, $dir if length($dir);
-    local $self->{_curline};
-    local $self->{_curfile} = $absfilename;
-    push @{$self->{_include_stack}}, $absfilename;
-    $self->{_include_level}++;
-    $self->_parse_raw($ct);
-    $self->{_include_level}--;
-    pop @{$self->{_include_stack}};
-
-    pop @CWD if length($dir);
+    {
+        my $tmp = sub {
+            local $self->{_curline};
+            local $self->{_curfile} = $absfilename;
+            push @{$self->{_include_stack}}, $absfilename;
+            $self->{_include_level}++;
+            $self->_parse_raw($ct);
+            $self->{_include_level}--;
+            pop @{$self->{_include_stack}};
+        };
+        $CWD = $dir if length($dir);
+        $tmp->();
+    }
 }
 
 sub dirmeta_include { {phase=>1} }
