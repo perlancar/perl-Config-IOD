895bf26440ff958c0ca1b6684aa02976aa808c44
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-10-20 19:41:51 +0700

    Fix typo

--- lib/Config/IOD.pm
+++ lib/Config/IOD.pm
@@ -1,4 +1,4 @@
-use Package::Alias 'Config::IOD' => 'Config::INI::OnDrugs';
+use Package::Alias 'Config::IOD' => 'Config::Ini::OnDrugs';
 package Config::IOD; # to make PodWeaver happy
 1;
 #ABSTRACT: Alias for Config::Ini::OnDrugs
